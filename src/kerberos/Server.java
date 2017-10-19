f5364ac53ffa1415930d7c013a452b502ea0d916
AnneGaelleC <anne-gaelle.calandre@telecomnancy.net>
2017-10-19 16:17:04 -0200

    Update Server.java

--- src/kerberos/Server.java
+++ src/kerberos/Server.java
@@ -18,25 +18,12 @@ import org.ietf.jgss.GSSName;
 import org.ietf.jgss.Oid;
 import sun.misc.BASE64Decoder;
  
-/**
- * <p>Server logs in to a Key Distribution Center (KDC) using JAAS and then
- * reads the encoded service ticket from the file <i>service-ticket.txt</i>.
- * This ticket is decoded into a byte array and the security context is
- * attempted to be accepted, using the GSS API.</p>
- * <p>This class, in combination with the <i>Client</i> class illustrates the
- * use of the JAAS and GSS APIs for initiating a security context using the
- * Kerberos protocol.</p>
- * <p>This requires a KDC/domain controller such as Active Directory or Apache
- * Directory. The KDC configuration details are stored in the
- * <i>server.properties</i> file, while the JAAS details are stored in the
- * file <i>jaas.conf</i>.</p>
- * @author Ants
- */
+
 public class Server {
  
   public static void main( String[] args) {
     try {
-      // Setup up the Kerberos properties.
+
       Properties props = new Properties();
       props.load( new FileInputStream( "/Users/anne-gaelle/Documents/Brésil/S2/SAS/KerberosV2/src/kerberos/server.properties"));
       System.setProperty( "sun.security.krb5.debug", "true");
@@ -48,7 +35,7 @@ public class Server {
       // Oid mechanism = use Kerberos V5 as the security mechanism.
       krb5Oid = new Oid( "1.2.840.113554.1.2.2");
       Server server = new Server();
-      // Login to the KDC.
+      // Login au KDC.
       server.login( password);
       byte serviceTicket[] = loadTokenFromDisk();
       // Request the service ticket.
@@ -73,7 +60,7 @@ public class Server {
     }
   }
  
-  // Load the security token from disk and decode it. Return the raw GSS token.
+  
   private static byte[] loadTokenFromDisk() throws IOException {
     BufferedReader in = new BufferedReader( new FileReader( "security.token"));
     System.out.println( new File( "security.token").getAbsolutePath());
@@ -92,26 +79,23 @@ public class Server {
  
   private Subject subject;
  
-  // Authenticate against the KDC using JAAS.
   private void login( String password) throws LoginException {
     LoginContext loginCtx = null;
-    // "Client" references the JAAS configuration in the jaas.conf file.
     loginCtx = new LoginContext( "Server",
         new LoginCallbackHandler( password));
     loginCtx.login();
     this.subject = loginCtx.getSubject();
   }
  
-  // Completes the security context initialisation and returns the client name.
   private String acceptSecurityContext( final byte[] serviceTicket)
       throws GSSException {
     krb5Oid = new Oid( "1.2.840.113554.1.2.2");
  
-    // Accept the context and return the client principal name.
+
     return Subject.doAs( subject, new PrivilegedAction<String>() {
       public String run() {
         try {
-          // Identify the server that communications are being made to.
+          // Identifie le server avec lequel les communications sont faites.
           GSSManager manager = GSSManager.getInstance();
           GSSContext context = manager.createContext( (GSSCredential) null);
           context.acceptSecContext( serviceTicket, 0, serviceTicket.length);
